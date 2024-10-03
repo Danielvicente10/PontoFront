b0f3cc35574cde857dc202bd93a108a1a361b24b
Daniel Vicente <danielvicente63@gmail.com>
2024-10-02 22:57:29 -0300

    Ajustes integração no back

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { NavBarVertical } from '../../Componentes/NavBar/Input';
-import './style.css';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { NavBarVertical } from "../../Componentes/NavBar/Input";
+import "./style.css";
 
 const Home: React.FC = () => {
-    const location = useLocation();
-    const userData = location.state?.userData;
-    console.log(userData)
-
-
-    return (
-        <div style={{display: "flex"}}>
-            <NavBarVertical></NavBarVertical>
-            {/* <MarkPointer></MarkPointer> */}
-            
-        </div>
-    );
+  const location = useLocation();
+  const userData = location.state?.userData;
+  return (
+    <div style={{ display: "flex" }}>
+      <NavBarVertical></NavBarVertical>
+      {/* <MarkPointer></MarkPointer> */}
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,65 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
-import { Input, InputPassword } from '../../Componentes/Inputs/Input';
-import { ButtonEnviar } from '../../Componentes/Buttons/buttons';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ButtonEnviar } from "../../Componentes/Buttons/buttons";
+import { Input, InputPassword } from "../../Componentes/Inputs/Input";
+import "./style.css";
 
 const Login: React.FC = () => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
-    useEffect(() => {
-        setIsButtonEnabled(name.trim() !== '' && password.trim() !== '');
-    }, [name, password]);
+  useEffect(() => {
+    setIsButtonEnabled(name.trim() !== "" && password.trim() !== "");
+  }, [name, password]);
+  const navigates = useNavigate();
+  const handleLogin = async () => {
+    const requestBody = { name, password };
 
-    const handleLogin = async () => {
-        const requestBody = { name, password };
+    try {
+      const response = await fetch("http://localhost:3030/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
 
-        try {
-            const response = await fetch('http://localhost:3030/user/insertUsers', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(requestBody)
-            });
+      const contentType = response.headers.get("Content-Type");
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      let data;
+      if (contentType && contentType.includes("application/json")) {
+        data = await response.json();
+      } else {
+        data = await response.text();
+      }
 
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('An error occurred:', error);
+      if (response.ok) {
+        if (data == "Error") {
+          return console.log("Senha invalida");
         }
-    };
+        navigate("/Home");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/Register");
+  };
 
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-      navigate('/Register');
-    };
-
-    return (
-        <div className='login-container'>
-            <div className='input-container'>
-            <h1>Login</h1>
-                <Input title="E-mail" value={name} onChange={(e) => setName(e.target.value)} />
-                <InputPassword title="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <div style={{marginTop: "-20px"}}>
-                    <span style={{fontSize: "12px"}}>Não tem uma conta <a className='register' onClick={handleClick}>Cadastre-se</a></span>
-                </div>
-                <ButtonEnviar
-                    className={`button ${isButtonEnabled ? 'enabled' : 'disabled'}`}
-                    disabled={!isButtonEnabled}
-                    onClick={handleLogin}
-                    title='Entrar'
-                />
-            </div>
+  return (
+    <div className="login-container">
+      <div className="input-container">
+        <h1>Login</h1>
+        <Input
+          title="E-mail"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <InputPassword
+          title="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div style={{ marginTop: "-20px" }}>
+          <span style={{ fontSize: "12px" }}>
+            Não tem uma conta{" "}
+            <a className="register" onClick={handleClick}>
+              Cadastre-se
+            </a>
+          </span>
         </div>
-    );
+        <ButtonEnviar
+          className={`button ${isButtonEnabled ? "enabled" : "disabled"}`}
+          disabled={!isButtonEnabled}
+          onClick={handleLogin}
+          title="Entrar"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
